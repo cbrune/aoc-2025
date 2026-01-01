c00f9549521e5191f8a608ec31e09b22c54b855e
Curt Brune <curt@brune.net>
2026-01-01 14:49:19 -0800

    day-9: part 2: works now
    it was not enough to test the points of the rect being inside the
    polygon.  need to test if the entire rect, including its interior, is
    inside the polygon.
    
    Signed-off-by: Curt Brune <curt@brune.net>

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,12 +1,10 @@
 // use std::collections::{BinaryHeap, HashSet};
-use std::fmt::Debug;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-use geo::{Contains, Intersects};
-use geo_types::{point, LineString, Polygon};
-use inpoly::inpoly2;
-use ndarray::{arr2, Array, Array2, ArrayView};
+use geo::Contains;
+use geo_types::{coord, LineString, Polygon, Rect};
+use ndarray::{Array, Array2, ArrayView};
 
 struct TilePointReader {
     lines: io::Lines<io::BufReader<File>>,
@@ -83,82 +81,6 @@ fn prob1(prob_file: &str) -> Result<f64, Box<dyn std::error::Error>> {
     Ok(max_area)
 }
 
-fn prob2(prob_file: &str) -> Result<f64, Box<dyn std::error::Error>> {
-    let array = data_init(prob_file)?;
-    let mut max_area = 0.;
-    //    println!("array: {array:?}");
-
-    //    let total_calcs = array.nrows() * (array.nrows() - 1);
-    //    println!("Total calcs: {total_calcs}");
-    let mut iter = 0;
-    let mut areas = Vec::new();
-    for i in 0..(array.nrows() - 1) {
-        for j in (i + 1)..array.nrows() {
-            //            if (iter % 1000) == 0 {
-            //                println!("Calc: {iter}/{total_calcs}");
-            //            }
-            //            iter += 1;
-            let x0 = array[[i, 0]];
-            let y0 = array[[i, 1]];
-            let x1 = array[[j, 0]];
-            let y1 = array[[j, 1]];
-
-            let delta_x = delta(x0, x1);
-            let delta_y = delta(y0, y1);
-            let area = delta_x * delta_y;
-            areas.push((x0, y0, x1, y1, area));
-        }
-    }
-
-    areas.sort_by(|a, b| {
-        let (_, _, _, _, a1) = a;
-        let (_, _, _, _, b1) = b;
-        let a1 = *a1 as usize;
-        let b1 = *b1 as usize;
-        b1.cmp(&a1)
-    });
-
-    //println!("areas: {areas:?}");
-
-    //    let total_calcs = areas.len();
-    //    let mut iter = 0;
-    //    println!("Total calcs2: {total_calcs}");
-    for (x0, y0, x1, y1, area) in areas {
-        //        if (iter % 1000) == 0 {
-        //            println!("Calc: {iter}/{total_calcs}");
-        //        }
-        //        iter += 1;
-        // test if rectangle defined by points is inside the boundary
-        // create array of points to check
-        let mut rect = Array::zeros((0, 2));
-        rect.push_row(ArrayView::from(&[x0, y0])).unwrap();
-        rect.push_row(ArrayView::from(&[x0, y1])).unwrap();
-        rect.push_row(ArrayView::from(&[x1, y1])).unwrap();
-        rect.push_row(ArrayView::from(&[x1, y0])).unwrap();
-
-        // println!("rect1: ({x0}, {y0}), ({x1}, {y1})");
-        // println!("rect: {rect:?}");
-
-        let (inside, on_boundry) = inpoly2(&rect, &array, None, None);
-        //            println!("Check: inside: {inside:?}");
-
-        // proceed if all inside
-        let inside2 = inside.iter().fold(true, |acc, e| acc && *e);
-        // let boundry = on_boundry.iter().fold(false, |acc, e| acc || *e);
-        //            println!("Final inside: {inside}");
-
-        if inside2 {
-            println!(
-                "Found max area: {area} - ({x0}, {y0}), ({x1}, {y1}) - {inside}, {on_boundry}, {iter}"
-            );
-            max_area = area;
-            break;
-        }
-    }
-
-    Ok(max_area)
-}
-
 fn data_init2(prob_file: &str) -> Result<Vec<(f64, f64)>, Box<dyn std::error::Error>> {
     let tile_point_reader = TilePointReader::new(prob_file)?;
 
@@ -172,8 +94,8 @@ fn data_init2(prob_file: &str) -> Result<Vec<(f64, f64)>, Box<dyn std::error::Er
     Ok(points)
 }
 
-fn prob3(prob_file: &str) -> Result<f64, Box<dyn std::error::Error>> {
-    let mut points = data_init2(prob_file)?;
+fn prob2(prob_file: &str) -> Result<f64, Box<dyn std::error::Error>> {
+    let points = data_init2(prob_file)?;
     let mut max_area = 0.;
 
     let line_string: LineString<f64> = points.clone().into();
@@ -211,36 +133,29 @@ fn prob3(prob_file: &str) -> Result<f64, Box<dyn std::error::Error>> {
 
     //    println!("areas: {areas:?}");
 
-    //    let total_calcs = areas.len();
+    let total_calcs = areas.len();
     let mut iter = 0;
     //    println!("Total calcs2: {total_calcs}");
     for (x0, y0, x1, y1, area) in areas {
-        //        if (iter % 1000) == 0 {
-        //            println!("Calc: {iter}/{total_calcs}");
-        //        }
+        if (iter % 1000) == 0 {
+            println!("Calc: {iter}/{total_calcs}");
+        }
         iter += 1;
         // test if rectangle defined by points is inside the boundary
         // create array of points to check
-        let mut rect = Vec::new();
-        rect.push(point!(x: x0, y: y0));
-        rect.push(point!(x: x0, y: y1));
-        rect.push(point!(x: x1, y: y1));
-        rect.push(point!(x: x1, y: y0));
+        let rect = Rect::new(coord! {x: x0, y: y0}, coord! {x: x1, y: y1});
 
-        /*
-                println!("rect: {rect:?}");
+        //        println!("rect: {rect:?}");
 
-                for p in &rect {
-                    let contains = poly.contains(p);
-                    println!("contains: p:{p:?}, {contains}");
-                    let intersects = poly.intersects(p);
-                    println!("intersects: p:{p:?}, {intersects}");
-                }
-        */
-        let inside = rect.iter().fold(true, |acc, e| acc && poly.intersects(e));
+        let contains = poly.contains(&rect);
+        //        println!("contains: rect:{rect:?}, {contains}");
+        // let intersects = poly.intersects(&rect);
+        //        println!("intersects: rect:{rect:?}, {intersects}");
 
-        if inside {
-            println!("Found max area: {area} - ({x0}, {y0}), ({x1}, {y1}) - {inside}, {iter}");
+        // let inside = rect.iter().fold(true, |acc, e| acc && poly.intersects(e));
+
+        if contains {
+            println!("Found max area: {area} - ({x0}, {y0}), ({x1}, {y1}) - {contains}, {iter}");
             max_area = area;
             break;
         }
@@ -260,9 +175,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let total = prob2(&input_file)?;
     println!("Part 2 - total: {total}");
 
-    let total = prob3(&input_file)?;
-    println!("Part 3 - total: {total}");
-
     Ok(())
 }
 
@@ -278,10 +190,5 @@ mod test {
     #[test]
     fn check_prob2() {
         assert_eq!(prob2("sample.txt").unwrap(), 24.);
-    }
-
-    #[test]
-    fn check_prob3() {
-        assert_eq!(prob3("sample.txt").unwrap(), 24.);
     }
 }
